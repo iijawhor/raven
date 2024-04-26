932469ed68bef45fc5a75519239f0573d5f8a143
ravenbusiness <ravenbusiness6@gmail.com>
2024-04-27 01:51:11 +0530

    Added Header file in  App.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { Header } from "./exports";
 function App() {
   return (
-    <div className="border-box p-0 m-0 relative h-full h-screen">
-      <h1 className="underline text-red-400 text-sans">ekart </h1>
+    <div className="border-box p-0 m-0 relative h-full h-screen bg-blue-900">
+      <Header />
     </div>
   );
 }
