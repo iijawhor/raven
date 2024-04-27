a289279922f23abf10f86029d065aae05dec68cb
ravenbusiness <ravenbusiness6@gmail.com>
2024-04-27 20:57:41 +0530

    changes in style

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,9 +3,9 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <section className="heroSection flex justify-center items-center relative">
-      <div className="delay-300 flex flex-col gap-3 z-10 text-center items-center text-white w-3/5">
+      <div className="flex flex-col gap-3 z-10 text-center items-center text-white lg:w-3/5 w-full px-2">
         <p className="text-sm tracking-widest text-white">CASUAL & EVERYDAY</p>
-        <h1 className="font-Cormorant tracking-wide italic text-7xl">
+        <h1 className="font-Cormorant tracking-wide italic text-4xl md:text-7xl">
           Effortlessly Blend <br /> Comfort &amp; Style!
         </h1>
         <p className="text-center leading-6 tracking-wide">
@@ -13,7 +13,7 @@ function HeroSection() {
           collection, featuring cozy sweaters, versatile <br /> denim, laid-back
           tees, and relaxed-fit joggers for your everyday adventures
         </p>
-        <button className="w-1/3 duration-300 capitalize text-lg cursor-pointer border border-white hover:bg-white hover:text-black py-2">
+        <button className="lg:w-1/3 mt-5 w-64 capitalize text-lg cursor-pointer border border-white hover:bg-white hover:text-black py-2">
           View Collection
         </button>
       </div>
