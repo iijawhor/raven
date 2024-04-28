6ac5686b80b80a5832906cbbdc38bb8b6aa3037c
ravenbusiness <ravenbusiness6@gmail.com>
2024-04-28 21:34:16 +0530

    Changes in style

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Banner() {
   return (
-    <section className="w-full md:h-screen relative py-20">
+    <section className="w-full md:h-screen relative">
       <div className="relative flex items-center justify-center">
         <div className="md:flex-1">
-          <div className="md:w-3/4 relative m-auto md:p-4 p-2 flex flex-col md:gap-2 items-start h-fit md:border-none border border-slate-100 left-5 bg-white rounded">
-            <p className="font-system text-xs font-semibold text-gray-500 tracking-wide">
+          <div className="md:w-3/4 md: relative h-full h-80  m-auto md:p-4 p-2 flex flex-col md:gap-2 items-start md:border-none border border-slate-100 left-5 bg-white rounded">
+            <p className="font-system text-xs font-semibold mt-1 text-gray-500 tracking-wide">
               WORK & OFFICE ATTIRE
             </p>
             <p className="text-wrap font-medium italic tracking-tight md:text-5xl text-xl font-Cormorant">
@@ -23,9 +23,9 @@ function Banner() {
             </button>
           </div>
         </div>
-        <div className="md:flex-1 md:h-2/4 w-72">
+        <div className="md:flex-1 w-72">
           <img
-            className="md:h-screen md:w-full bg-cover bg-center h-96 min-w-40"
+            className="md:w-full bg-cover bg-center max-h-[100vh] min-h-[55vh] min-w-40"
             src="https://d2w9m16hs9jc37.cloudfront.net/dimg/blog/2022/11/imagetools3.jpg"
             alt=""
           />
